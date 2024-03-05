c8e23539bf96a06cd55130ad803d15eb465ec876
russelparumog22 <russelparumog515@gmail.com>
2024-03-05 16:23:22 +0800

    added

---
+++ register.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+console.error("Error Happened!");
+console.warn("Warning");
+
+// Variables
+// string = "Words" , 'wowwoo', '123'
+// number = 123, 434, 2398.12
+// boolean = true, false
+// object = ["apple" , "banana"], [1,2,3,4]
+            //["apple", 3, true]
+// typeof
+// console.log(typeof(["apple","banana"]))
+
+//  Declaration of Variables 
+let name = "Russel"
+lastname = `Parumog`
+age = 10;
+isProf =  true,
+hobbies = ["Watch tv" , "Play Video Games"],
+run = function () {
+    console.log("Im running");
+
+};
+console.log(name, age, isProf, hobbies, run);
+console.log(typeof run);
+console.table(hobbies);
+console.log("Myname is" + name + " " + lastname);
+console.log(`Myname is ${name} ${lastname}`);
+
